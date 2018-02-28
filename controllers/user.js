0377c4e0d3ed8fa2339ba5377a60f0af99121c44
franciscocamposchimal <camposfranciscoing@gmail.com>
2018-02-27 18:06:06 -0600

    new url

--- controllers/user.js
+++ controllers/user.js
@@ -76,7 +76,7 @@ module.exports = {
 
     getAll: async (req,res,next) => {
         const user_id = parseInt(req.params.id);
-        const findDevices = await User.find({user_id: user_id});
+        const findDevices = await User.find({user_id: user_id},'device');
         if(findDevices == 1)
             return res.status(200).json({devices:findDevices});
         else
